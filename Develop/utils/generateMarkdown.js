0008d52e11a680e4c272f81f75df7e6f88d25b1f
unknown <rgarcia20.m@gmail.com>
2023-05-07 11:51:11 -0700

    created function for readmefile contents, github markdown, and email address

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,7 +6,7 @@ function renderLicenseBadge(license) {
     return "";
 
 }
-// Function that takes in a license string as a paramert and retuns a link to the correspinding license page
+// Function that takes in a license string as a parameter and retuns a link to the corresponding license page
 function renderLicenseLink(license) {
     if (license !== "None") {
         return (
@@ -14,4 +14,51 @@ function renderLicenseLink(license) {
         );
     }
     return "";
+}
+// Function created to take in a license string as a parameter and returns a Markdown section with information
+// about the license
+function renderLicenseSection(license) {
+    if (license !== "None") {
+        return `## License
+        This project is licensed under the ${license} license. To learn more please visit ${renderLicenseLink(license)}.`;
+         }
+         return "";
+    }
+    // function created to take in an email address as a parameter and retun as a markdown link that will open the user's email
+    // with a new email addressed to the provided email
+    function renderMailTo(email) {
+        return `[${email}](mailto:${email})`;
+    }
+    // function takes in a Github username as a parameter and returns a Markdown link to the user's Github Profile
+    function renderGitHubLink(github) {
+        return `[${github}](github.com/${github})`;
+    }
+    // function to generate the README file contents
+    function generateMarkdown(data) {
+        return `# ${data.title}
+
+        ${renderLicenseBadge(data.license)}
+
+        # Description
+
+        ${data.description}
+
+        # Table of Contents
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [Contributing](#credits)
+        - [License](#license)
+        - [Tests](#tests)
+        - [Questions](#questions)
+
+        # Installation
+
+        ${data.installation}
+
+
+
+
+    }
+
+
 }
